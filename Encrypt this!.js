d01089b5b66edb07d1b965f4ff6c1e877fa7f46b
tchrelouch <128306457+tchrelouch@users.noreply.github.com>
2023-03-19 20:06:16 +0400

    Create Encrypt this!.js

---
+++ Encrypt this!.js
@@ -0,0 +1,17 @@
+// Description:
+// Encrypt this!
+
+// You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+
+// Your message is a string containing space separated words.
+// You need to encrypt each word in the message using the following rules:
+// The first letter must be converted to its ASCII code.
+// The second letter must be switched with the last letter
+// Keepin' it simple: There are no special characters in the input.
+// Examples:
+// encryptThis("Hello") === "72olle"
+// encryptThis("good") === "103doo"
+// encryptThis("hello world") === "104olle 119drlo"
+let encryptThis = function(text) {
+  return text.replace(/\b\w/g, (w=> w.charCodeAt())).split(" ").map(w=> w.replace(/([a-z])(.*)(\w\b)/i, "$3$2$1")).join(" ")
+}
