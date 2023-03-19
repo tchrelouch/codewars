a3db6be554c26ced9ef87ca4eaadb1e3f6493e88
tchrelouch <128306457+tchrelouch@users.noreply.github.com>
2023-03-19 19:12:21 +0400

    Create Reverse every other word in the string.js

---
+++ Reverse every other word in the string.js
@@ -0,0 +1,7 @@
+/*
+Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, 
+while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+*/
+function reverse(str){
+  return /^\s+$/.test(str) ? "" : str.split(" ").map((word,index)=>index%2!==0?word.split("").reverse().join(""):word).join(" ")
+}
