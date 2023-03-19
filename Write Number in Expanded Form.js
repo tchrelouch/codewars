7d9bcb2a4fd6cd6b226ee64f209677d0ea1778d9
tchrelouch <128306457+tchrelouch@users.noreply.github.com>
2023-03-19 20:13:00 +0400

    Create Write Number in Expanded Form.js

---
+++ Write Number in Expanded Form.js
@@ -0,0 +1,13 @@
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// NOTE: All numbers will be whole numbers greater than 0.
+function expandedForm(num) {
+  return (''+num).split("")
+            .map((n,i)=> {if(n != 0) return n + "0".repeat((''+num).length - i - 1)})
+            .filter(e => typeof e === "string")
+            .join(" + ");
+}
